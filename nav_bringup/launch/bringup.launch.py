2ee0b4026fc08b6c4fb5c7853c6478fbfc4970af
Zhiang QI <an9qi@yandex.com>
2024-12-14 02:19:17 +0800

    修复成本地图“清除”机制错误，取消使用pcl2laserscan，用voxel_layer代替，使用实际机器人的urdf

--- nav_bringup/launch/bringup.launch.py
+++ nav_bringup/launch/bringup.launch.py
@@ -49,27 +49,6 @@ def generate_launch_description():
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([lidar_localization_dir, '/launch/lidar_localization.launch.py']),
                 launch_arguments={'use_sim_time': use_sim_time}.items()
-            ),
-            Node(
-                package='pointcloud_to_laserscan',
-                executable='pointcloud_to_laserscan_node',
-                remappings=[('cloud_in', ['/livox/lidar/pointcloud']),
-                            ('scan', ['/scan'])],
-                parameters=[{
-                    'target_frame': 'chassis',
-                    'transform_tolerance': 0.01,
-                    'min_height': 0.0,
-                    'max_height': 2.0,
-                    'angle_min': -1.5708,  # -M_PI/2
-                    'angle_max': 2.5708,  # M_PI/2
-                    'angle_increment': 0.0087,  # M_PI/360.0
-                    'scan_time': 0.3333,
-                    'range_min': 0.05,
-                    'range_max': 10.0,
-                    'use_inf': True,
-                    'inf_epsilon': 1.0
-                }],
-                name='pointcloud_to_laserscan'
             )
         ]
     )
--- pointcloud_to_laserscan/launch/sample_pointcloud_to_laserscan_launch.py
+++ pointcloud_to_laserscan/launch/sample_pointcloud_to_laserscan_launch.py
@@ -14,13 +14,13 @@ def generate_launch_description():
                 'target_frame': 'chassis',
                 'transform_tolerance': 0.01,
                 'min_height': 0.0,
-                'max_height': 1.0,
+                'max_height': 0.65,
                 'angle_min': -1.5708,  # -M_PI/2
                 'angle_max': 1.5708,  # M_PI/2
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 0.45,
-                'range_max': 10.0,
+                'range_min': 0.05,
+                'range_max': 5.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
