0f750f96fa3e3a6ad44d7f83be6624c516757b68
Zhiang QI <an9qi@yandex.com>
2024-12-17 10:54:21 +0800

    add sim option

--- nav_bringup/launch/sim_nav_bringup.launch.py
+++ nav_bringup/launch/sim_nav_bringup.launch.py
@@ -100,12 +100,12 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='false',
+        default_value='true',
         description='Use simulation (Gazebo) clock if true')
 
     declare_params_file_cmd = DeclareLaunchArgument(
         'params_file',
-        default_value=os.path.join(bringup_dir, 'params', 'nav2_params.yaml'),
+        default_value=os.path.join(bringup_dir, 'params', 'sim_nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
 
     declare_autostart_cmd = DeclareLaunchArgument(
