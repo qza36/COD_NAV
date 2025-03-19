a1a88f2cb02da1bcc19f6a0c348a9f3b083f55f2
ZhiangQi <an9qi@yandex.com>
2025-03-19 20:50:54 +0800

    update command velocity topic and adjust map paths in configuration files

---
+++ nav_bringup/launch/lio_only.launch.py
@@ -0,0 +1,93 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution,Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # 获取包的共享目录
+    fastlio_dir = get_package_share_directory('fast_lio')
+    lidar_localization_dir = get_package_share_directory('lidar_localization_ros2')
+    #terrrain_analysis_dir = get_package_share_directory('terrain_analysis')
+
+    # 配置文件路径
+    fastlio_config_path = os.path.join(fastlio_dir, 'config')
+    fast_lio_config_file = 'mid360.yaml'
+    robot_description = Command(['xacro ', os.path.join(
+        get_package_share_directory('nav_bringup'), 'urdf', 'simulation_waking_robot.xacro')])
+
+    # 声明启动参数
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time', default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # 定义节点和包含的launch文件
+    load_nodes = GroupAction(
+        actions=[
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                parameters=[{
+                    'robot_description': robot_description
+                }],
+                output='screen'
+            ),
+            Node(
+                package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+                remappings=[('cloud_in',  '/livox/lidar/pointcloud'),
+                            ('scan', '/scan')],
+                parameters=[{
+                    'target_frame': 'chassis',
+                    'transform_tolerance': 0.01,
+                    'min_height': 0.0,
+                    'max_height': 0.65,
+                    'angle_min': -3.1416,  # -M_PI/2
+                    'angle_max': 3.1416,  # M_PI/2
+                    'angle_increment': 0.0087,  # M_PI/360.0
+                    'scan_time': 0.3333,
+                    'range_min': 0.05,
+                    'range_max': 5.0,
+                    'use_inf': True,
+                    'inf_epsilon': 1.0
+                }],
+                name='pointcloud_to_laserscan'
+            ),
+            Node(
+                package='fast_lio',
+                executable='fastlio_mapping',
+                parameters=[PathJoinSubstitution([fastlio_config_path, fast_lio_config_file]),
+                            {'use_sim_time': use_sim_time}],
+                output='screen'
+            ),
+            # IncludeLaunchDescription(
+            #     PythonLaunchDescriptionSource([lidar_localization_dir, '/launch/sim_lidar_localization.launch.py']),
+            #     launch_arguments={'use_sim_time': use_sim_time}.items()
+            # ),
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                arguments=[
+                    # Useless arguments, provided by LIO in publish_odometry() function
+                    '--x', '0.0',
+                    '--y', '0.0',
+                    '--z', '0.0',
+                    '--roll', '0.0',
+                    '--pitch', '0.0',
+                    '--yaw', '0.0',
+                    '--frame-id', 'map',
+                    '--child-frame-id', 'odom'
+                ],
+            ),
+
+        ]
+    )
+
+    return LaunchDescription([
+        declare_use_sim_time,
+        load_nodes
+    ])
