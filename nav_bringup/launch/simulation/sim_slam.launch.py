950d3cb03d67005ca157eb5898cff40d6298ebc4
Zhiang QI <an9qi@yandex.com>
2025-03-20 20:56:15 +0800

    refactor SLAM configuration and launch files for improved parameter management and async processing

--- nav_bringup/launch/simulation/sim_slam.launch.py
+++ nav_bringup/launch/simulation/sim_slam.launch.py
@@ -9,24 +9,54 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     # 获取包的共享目录
     fastlio_dir = get_package_share_directory('fast_lio')
+    bringup_dir = get_package_share_directory('nav_bringup')
     # 配置文件路径
     fastlio_config_path = os.path.join(fastlio_dir, 'config')
     fast_lio_config_file = 'mid360.yaml'
 
     slam_params_file = LaunchConfiguration('slam_params_file')
 
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+
 
 # 声明启动参数
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(bringup_dir, 'params', 'nav2_slam_params.yaml'),
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
     declare_use_sim_time = DeclareLaunchArgument(
-        'use_sim_time', default_value='true',
+        'use_sim_time', default_value='True',
         description='Use simulation (Gazebo) clock if true')
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
         default_value=os.path.join(get_package_share_directory("nav_bringup"),
-                                   'params', 'mapper_params_localization.yaml'),
+                                   'params', 'mapper_params_async.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
-
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        'use_respawn', default_value='False',
+        description='Whether to respawn if a node crashes. Applied when composition is disabled.')
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the nav2 stack')
+    declare_log_level_cmd = DeclareLaunchArgument(
+        'log_level', default_value='info',
+        description='log level')
+#获得启动参数
+    use_respawn = LaunchConfiguration('use_respawn')
     use_sim_time = LaunchConfiguration('use_sim_time')
+    configured_params = LaunchConfiguration('params_file')
+    autostart = LaunchConfiguration('autostart')
+    log_level = LaunchConfiguration('log_level')
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    lifecycle_nodes = ['controller_server',
+                       'smoother_server',
+                       'planner_server',
+                       'behavior_server',
+                       'bt_navigator',
+                       'waypoint_follower',
+                       'velocity_smoother']
 
     # 定义节点和包含的launch文件
     load_nodes = GroupAction(
@@ -58,25 +88,104 @@ def generate_launch_description():
                             {'use_sim_time': use_sim_time}],
                 output='screen'
             ),
-            # IncludeLaunchDescription(
-            #     PythonLaunchDescriptionSource([lidar_localization_dir, '/launch/sim_lidar_localization.launch.py']),
-            #     launch_arguments={'use_sim_time': use_sim_time}.items()
-            # ),
             Node(
                 parameters=[
                     slam_params_file,
                     {'use_sim_time': use_sim_time}
                 ],
                 package='slam_toolbox',
-                executable='localization_slam_toolbox_node',
+                executable='async_slam_toolbox_node',
                 name='slam_toolbox',
-                output='screen')
+                output='screen'),
+            Node(
+                package='nav2_controller',
+                executable='controller_server',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings+[('cmd_vel', 'cmd_vel_nav')]),
+            Node(
+                package='nav2_smoother',
+                executable='smoother_server',
+                name='smoother_server',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings),
+            Node(
+                package='nav2_planner',
+                executable='planner_server',
+                name='planner_server',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings),
+            Node(
+                package='nav2_behaviors',
+                executable='behavior_server',
+                name='behavior_server',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings),
+            Node(
+                package='nav2_bt_navigator',
+                executable='bt_navigator',
+                name='bt_navigator',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings),
+            Node(
+                package='nav2_waypoint_follower',
+                executable='waypoint_follower',
+                name='waypoint_follower',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings),
+            Node(
+                package='nav2_velocity_smoother',
+                executable='velocity_smoother',
+                name='velocity_smoother',
+                output='screen',
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=['--ros-args', '--log-level', log_level],
+                remappings=remappings +
+                           [('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_navigation',
+                output='screen',
+                arguments=['--ros-args', '--log-level', log_level],
+                parameters=[{'use_sim_time': use_sim_time},
+                            {'autostart': autostart},
+                            {'node_names': lifecycle_nodes}]),
         ]
     )
     ld = LaunchDescription()
 
     ld.add_action(declare_use_sim_time)
+    ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_log_level_cmd)
     ld.add_action(load_nodes)
 
     return ld
